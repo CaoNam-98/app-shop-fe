32d41e97ada0466bdbe24474fe9c05f0daabc8b3
Cao Nam <caonam.uit@gmail.com>
2024-06-20 23:03:55 +0700

    Update code login

---
+++ src/views/pages/login/index.tsx
@@ -0,0 +1,12 @@
+// ** Import Next
+import { NextPage } from 'next'
+
+type TProps = {}
+
+const LoginPage: NextPage<TProps> = () => {
+  return (
+    <h1>Login Page</h1>
+  )
+}
+
+export default LoginPage;
